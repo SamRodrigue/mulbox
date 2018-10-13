1ae41eb41d6bac3f04ffa45d3612622e575bfa44
Sam Rodrigue <samrodrigue@hotmail.com>
2018-10-13 18:32:55 -0400

    Added test add and remove boxes. Linked cdn for jQuery and mulbox script

---
+++ public/javascripts/mulbox.js
@@ -0,0 +1,37 @@
+var charLimit = 10;
+
+$(document).ready(function() {
+  // Add box
+  function addBox(id) {
+    // Get the next box
+    var $box = $("#" + id);
+
+    // Check if box exists
+    if (!$box.length) {
+      // Add a box
+      $("#mulboxes").append('<textarea class="mulbox" id="' + id + '" maxlength="140" rows="10" cols="30" placeholder="Write some shit here! "></textarea>');
+    }
+  }
+
+  // Remove box
+  function removeBox(id) {
+    // Get the next box
+    var $box = $("#" + id);
+
+    // Check if box exists
+    if ($box.length === 1) {
+      // Add a box
+      $box.remove();
+    }
+  }
+
+  $("#addBox").click(function(event) {
+    var boxes = $(".mulbox").length
+    addBox(boxes + 1);
+  });
+
+  $("#removeBox").click(function(event) {
+    var boxes = $(".mulbox").length
+    removeBox(boxes);
+  });
+});
