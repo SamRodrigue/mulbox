31316670464c6fb239b7bb1b882ba6fa0ad5fb4f
Sam Rodrigue <samrodrigue@hotmail.com>
2019-02-02 10:19:46 -0500

    New input method demo, with single input.

--- public/javascripts/mulbox.js
+++ public/javascripts/mulbox.js
@@ -3,290 +3,67 @@ var charLimit = 32;
   .ready waits for the DOM to be fully loaded
 */
 
-var lastKey = null;
-
 $(document).ready(function() {
-  // mulbox key listener
-
-  $("#mulboxes").on("keydown", ".mulbox", function(event) {
-    lastKey = event.keyCode;
+  // inbox key listener
+  $("#inbox").on("input", function(event) {
+    updateBoxes();
   });
 
-  $("#mulboxes").on("keyup", ".mulbox", function() {
-    lastKey = null;
-  });
+  function updateBoxes() {
+    // Remove old mulboxes
+    $(".mulboxWrapper").remove();
 
-  $("#mulboxes").on("input", ".mulbox", function(event) {
-    var box = event.originalEvent.target;
-    var id = box.id;
+    var inbox = $("#inbox")[0];
+    var input = inbox.value;
 
-    updateBox(id);
-  });
+    var start = 0;
+    var index = 1;
+    while (start < input.length) {
+      var pager = " (" + index + ")";
+      var boxLimit = charLimit - pager.length;
+      var subInput = input.substr(start, boxLimit);
 
-  // Update box
-  function updateBox(id) {
-    // Get the mulbox
-    var box = getBox(id)[0];
+      if (start + subInput.length !== input.length) {
+        var lastBreak = subInput.lastIndexOf(" ");
 
-    var chars = box.value;
-
-    if (chars.length === 0) {
-      if (id != 1) {
-        removeBox(id);
-      }
-      return;
-    }
-
-    var cursor = box.selectionEnd;
-    console.log("Key code: " + lastKey);
-
-    if (lastKey === 8 && cursor === 0) { // Backspace
-      // Move cursor to previous box
-      if (id > 1) {
-        var previd = parseInt(id) - 1
-        var prev = getBox(previd)[0];
-        prev.focus();
-        prev.selectionEnd = prev.value.length;
-        prev.selectionStart = prev.value.length;
-        return;
-      }
-    }
-
-    // if (lastKey === 46 && cursor === box.value.length) { // Delete
-    //   // Check if next box is available
-    //   var nextid = parseInt(id) + 1;
-    //   if (checkBox(nextid)) {
-    //     var next = getBox(nextid)[0];
-    //   }
-    // }
-
-    var words = chars.split(" ");
-
-    console.log("id:" + id + " chars:" + chars.length + " words:" + words.length);
-
-    // Check if characters exceeds limits
-    // TODO: Account for page numbering
-    if (chars.length > charLimit) {
-      console.log("number of characters exceeded in mulbox id:" + id);
-      
-      // Determine which characters need to move to the next box
-      var move = "";
-      var fullWord = true;
-      // Special case if the first word exceeds the character limit
-      if (words[0].length > charLimit) {
-        fullWord = false;
-        move = chars.substring(charLimit, chars.length);
-      } else {
-        // Starting at charLimit back track to previous ' '
-        for (var cut = charLimit; cut >= 0; --cut) {
-          var char = chars[cut];
-          if (char === ' ') {
-            move = chars.substring(cut + 1, chars.length);
-            break;
-          }
+        if (lastBreak > -1) {
+          subInput = input.substr(start, lastBreak);
+          start++;
         }
       }
 
-      // Trim current box
-      console.log("Trim chars:" + chars.length + " move:" + move.length);
-      if (fullWord) {
-        box.value = chars.substring(0, chars.length - move.length - 1);
-      } else {
-        box.value = chars.substring(0, chars.length - move.length);
-      }
-
-      // Check if next box is available
-      var nextid = parseInt(id) + 1;
-      if (!checkBox(nextid)) {
-        addBox(nextid);
-      }
-      var next = getBox(nextid);
-
-      if (next.val().length === 0) {
-        next.val(move);
-      } else {
-        next.val(move + " " + next.val());
-      }
-
-      // Move cursor
-      // Check if cursor is at the end of text area
-      if (cursor >= box.value.length) {
-        next[0].focus();
-        next[0].selectionEnd = move.length;
-      } else {
-        box.selectionEnd = cursor;
-      }
-
-      updateBox(nextid);
-
-    } else {
-      // Check if first word from next box can be brought to the current box
-
-      // Check if next box is available
-      var nextid = parseInt(id) + 1;
-      if (!checkBox(nextid)) {
-        // No other boxes
-        return;
-      }
-      var next = getBox(nextid)[0];
-
-      // Get first word from next box
-      var nextChars = next.value;
-      var nextMove = "";
-
-      // Check if whole text can be taken from next box
-      if (chars.length + nextChars.length + 1 <= charLimit) {
-        box.value = box.value + " " + next.value;
-        next.value = "";
-        box.selectionEnd = cursor;
-
-        updateBox(nextid);        
-      } else {
-        for (var cut = charLimit - chars.length - 1; cut >= 0; --cut) {
-          var char = nextChars[cut];
-          if (char === ' ') {
-            nextMove = nextChars.substring(0, cut);
-            break;
-          }
-        }
-
-        if (nextMove.length > 0) {
-          box.value = box.value + " " + nextMove;
-          next.value = next.value.substring(cut + 1);
-          box.selectionEnd = cursor;
-
-          updateBox(nextid);
-        }
-      }
+      addBox(index, subInput + pager);
+      index++;
+      start += subInput.length;
     }
   }
 
   // Add box
-  function addBox(id) {
-    // Box id 1 should not be altered
-    if (id === 1) {
-      console.log("ERROR: Attempting to add mulbox with id:" + id);
-      return;
-    }
-
-    // Get the mulbox
-    var $box = checkBox(id);
-    var $above = getBox(id - 1);
-
-    if ($above === null) {
-      // TODO RECOVERY: Find next above box, change id to above + 1;
-      console.log("ERROR: Unable to add mulbox with id:" + id + ", no above mulbox with id:" + (id - 1));
-
-      return null;
-    }
-
-    // Check if box exists
-    if ($box !== null) {
-      // mulbox with id already exists, shift existing boxes down
-      var $mulboxes = $(".mulbox");
-
-      for (var i = id; i <= $mulboxes.length; ++i) {
-        $mulboxes[i - 1].id = i + 1;
-      }
-    }
-
-    //converts id to base 16 hexadecimal unicode needed for copy function
-      function getUniId(id) {
-        var idString = String(id);
-        var idHex = "";
-  
-        for (var i = 0; i < idString.length; ++i) {
-          idHex += "\\" + idString.charCodeAt(i).toString(16);
-        }
-  
-        return idHex;
-      }
+  function addBox(id, value) {
+    // Get unicode id
     var uniId = getUniId(id);
+
     // Add a box
-    $box = $above.parent(".mulboxWrapper").after(`
+    $("#mulboxes").append(`
       <div class="mulboxWrapper">
-        <textarea class="mulbox" id="` + id + `" maxlength="140" rows="10" cols="30" placeholder="Write some shit here! "></textarea>
+        <textarea disabled class="mulbox" id="` + id + `" maxlength="140" rows="10" cols="30">` + value + `</textarea>
         <button class="btn btnCopy" data-clipboard-action="copy" data-clipboard-target="#`+ uniId +`">
           <i class="fas fa-copy"></i>
         </button>
       </div>`);
-
-    return $box;
   }
 
-  // Remove box
-  function removeBox(id) {
-    // Box id 1 should not be altered
-    if (id === 1) {
-      console.log("ERROR: Attempting to add mulbox with id:" + id);
-      return;
+  //converts id to base 16 hexadecimal unicode needed for copy function
+  function getUniId(id) {
+    var idString = String(id);
+    var idHex = "";
+
+    for (var i = 0; i < idString.length; ++i) {
+      idHex += "\\" + idString.charCodeAt(i).toString(16);
     }
 
-    // Get the mulbox
-    var $box = getBox(id);
-
-    if ($box !== null) {
-      // Remove parent
-      $box.parent(".mulboxWrapper").remove();
-
-      // Shift remaining mulboxes up
-      var $mulboxes = $(".mulbox");
-
-      for (var i = id; i <= $mulboxes.length; ++i) {
-        $mulboxes[i - 1].id = i;
-      }
-    }
+    return idHex;
   }
-
-  function getBox(id) {
-    var $box = $("#" + id);
-
-    // Check if box exists
-    if ($box.length > 1) {
-      // More than one box
-      console.log("ERROR: Multiple mulboxes with id:" + id + " exist");
-    } else if ($box.length === 0) {
-      // No box with id found
-      console.log("ABN: Zero mulboxes found with id:" + id + " exists");
-    } else {
-      return $box;
-    }
-
-    // NOK
-    return null;
-  }
-
-  function checkBox(id) {
-    var $box = $("#" + id);
-
-    // Check if box exists
-    if ($box.length === 1) {
-      return $box;
-    }
-
-    return null;
-  }
-
-  // Test buttons to add and remove mulboxes
-  $("#addBox").click(function(event) {
-    var boxes = $(".mulbox").length
-    addBox(boxes + 1);
-  });
-
-  $("#removeBox").click(function(event) {
-    var boxes = $(".mulbox").length
-    removeBox(boxes);
-  });
-
-  $("#addBox2").click(function(event) {
-    var boxes = $(".mulbox").length
-    addBox(2);
-  });
-
-  $("#removeBox2").click(function(event) {
-    var boxes = $(".mulbox").length
-    removeBox(2);
-  });
 
 //Modal 
   var modal = document.getElementById('modal');
@@ -323,19 +100,17 @@ $(document).ready(function() {
         toggleWrap.style.clipPath = 'inset(0 50% 0 0)';
         toggleWrap.style.backgroundColor = '#0084b4';
       }
-    console.log(toggleNumber)
-   });
+    console.log(toggleNumber);
+  });
 
-   var clipboard = new ClipboardJS('.btnCopy');
+  var clipboard = new ClipboardJS('.btnCopy');
 
    
-   clipboard.on('success', function(e) {
-      console.log(e);
-    });
+  clipboard.on('success', function(e) {
+    console.log(e);
+  });
 
-    clipboard.on('error', function(e) {
-      console.log(e);
-    });
-
-
+  clipboard.on('error', function(e) {
+    console.log(e);
+  });
 });
